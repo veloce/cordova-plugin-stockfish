06cc58d03351bee0ee61835772f834f5b20dfb2d
Vincent Velociter <vincent.velociter@gmail.com>
2015-09-25 16:19:04 +0200

    Change param order

--- www/CordovaPluginStockfish.js
+++ www/CordovaPluginStockfish.js
@@ -2,7 +2,7 @@ module.exports.init = function (success, error) {
   cordova.exec(success, error, 'CordovaPluginStockfish', 'init', []);
 };
 
-module.exports.cmd = function (success, error, cmd) {
+module.exports.cmd = function (cmd, success, error) {
   if (typeof cmd !== 'string') {
     error('cmd must be called with a single string parameter.');
   } else {
