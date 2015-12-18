52c6f16da0dbf5ce7a4b93fbd1a50ea391400068
Sebastien Renault <srenault.contact@gmail.com>
2015-12-18 17:21:24 +0100

    Read stdout every time

--- src/android/jni/stockfishjni/stockfishjni.cpp
+++ src/android/jni/stockfishjni/stockfishjni.cpp
@@ -14,6 +14,33 @@ extern "C" {
 
 bool run = false;
 
+auto readstdout = []() {
+  std::streambuf* out = std::cout.rdbuf();
+  std::ostringstream lichout;
+  std::cout.rdbuf(lichout.rdbuf());
+
+  LOGD("stockfishcli", "##> UP");
+
+  run = true;
+
+  while(run) {
+    std::string output = lichout.str();
+    lichout.str("");
+
+    std::istringstream lichin(output);
+    char line[1024];
+    lichin.getline(line, 1024);
+
+    if(strlen(line) > 0) {
+      LOGD("stockfishcli", "##> %s", line);
+    }
+  };
+
+  std::cout.rdbuf(out);
+};
+
+std::thread reader (readstdout);
+
 JNIEXPORT void JNICALL Java_org_lichess_stockfish_CordovaPluginStockfishJNI_init(JNIEnv *env, jclass clazz) {
   UCI::init(Options);
   PSQT::init();
@@ -26,24 +53,6 @@ JNIEXPORT void JNICALL Java_org_lichess_stockfish_CordovaPluginStockfishJNI_init
   Threads.init();
   Tablebases::init(Options["SyzygyPath"]);
   TT.resize(Options["Hash"]);
-
-  auto readstdout = []() {
-    std::streambuf* out = std::cout.rdbuf();
-    std::stringstream inout;
-    std::cout.rdbuf(inout.rdbuf());
-
-    run = true;
-
-    while(run) {
-      char line[256];
-      inout.getline(line, 256);
-      LOGD("stockfishcli", "##> %s", line);
-    }
-
-    std::cout.rdbuf(out);
-  };
-
-  std::thread reader (readstdout);
 }
 
 JNIEXPORT void JNICALL Java_org_lichess_stockfish_CordovaPluginStockfishJNI_exit(JNIEnv *env, jclass clazz) {
@@ -52,6 +61,7 @@ JNIEXPORT void JNICALL Java_org_lichess_stockfish_CordovaPluginStockfishJNI_exit
 
 JNIEXPORT void JNICALL Java_org_lichess_stockfish_CordovaPluginStockfishJNI_cmd(JNIEnv *env, jclass clazz, jstring jcmd) {
   const char *cmd = env->GetStringUTFChars(jcmd, (jboolean *)0);
+  LOGD("stockfishcli", "cmd %s", cmd);
   stockfishcli::commandInit();
   stockfishcli::command(cmd);
   env->ReleaseStringUTFChars(jcmd, cmd);
