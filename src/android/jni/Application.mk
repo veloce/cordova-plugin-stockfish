4887c6b27c51740a13467ee838d6c3af46a89b7d
Sebastien Renault <srenault.contact@gmail.com>
2016-04-24 22:13:04 +0200

    Android platform: compile for x86

--- src/android/jni/Application.mk
+++ src/android/jni/Application.mk
@@ -1,6 +1,6 @@
 APP_PLATFORM := android-16
 APP_STL := c++_shared
-APP_ABI := armeabi-v7a
+APP_ABI := armeabi-v7a x86
 APP_OPTIM := release
 APP_CPPFLAGS += -std=c++11 -fexceptions -DHAVE_PTHREAD=1
 APP_USE_CPP0X := true
