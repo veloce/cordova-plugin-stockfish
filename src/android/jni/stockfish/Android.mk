7df960ac0322150db69826db1ae3cb7eddaca444
Vincent Velociter <vincent.velociter@gmail.com>
2018-04-26 14:58:12 +0200

    Add support for cordova-android 7.0 and gradle

--- src/android/jni/stockfish/Android.mk
+++ src/android/jni/stockfish/Android.mk
@@ -3,25 +3,25 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 stockfish_src_files := \
-    benchmark.cpp                                                \
-    bitbase.cpp                                                  \
-    bitboard.cpp                                                 \
-    endgame.cpp                                                  \
-    evaluate.cpp                                                 \
-    material.cpp                                                 \
-    misc.cpp                                                     \
-    movegen.cpp                                                  \
-    movepick.cpp                                                 \
-    pawns.cpp                                                    \
-    position.cpp                                                 \
-    psqt.cpp                                                     \
-    search.cpp                                                   \
-    thread.cpp                                                   \
-    timeman.cpp                                                  \
-    tt.cpp                                                       \
-    uci.cpp                                                      \
-    ucioption.cpp                                                \
-    syzygy/tbprobe.cpp                                           \
+    src/benchmark.cpp                             \
+    src/bitbase.cpp                               \
+    src/bitboard.cpp                              \
+    src/endgame.cpp                               \
+    src/evaluate.cpp                              \
+    src/material.cpp                              \
+    src/misc.cpp                                  \
+    src/movegen.cpp                               \
+    src/movepick.cpp                              \
+    src/pawns.cpp                                 \
+    src/position.cpp                              \
+    src/psqt.cpp                                  \
+    src/search.cpp                                \
+    src/thread.cpp                                \
+    src/timeman.cpp                               \
+    src/tt.cpp                                    \
+    src/uci.cpp                                   \
+    src/ucioption.cpp                             \
+    src/syzygy/tbprobe.cpp                        \
 
 LOCAL_MODULE := stockfish
 LOCAL_CPP_EXTENSION := .cpp
--- src/android/jni/stockfishjni/Android.mk
+++ src/android/jni/stockfishjni/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := stockfishjni
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../stockfish/
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../stockfish/src/
 LOCAL_SHARED_LIBRARIES := stockfish
 LOCAL_SRC_FILES := stockfishjni.cpp
 LOCAL_LDLIBS := -llog -landroid
